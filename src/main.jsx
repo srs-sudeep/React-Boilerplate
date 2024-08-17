9f24fa27df0848576b160cb08e73f0fb70a4d097
suprabhosaha <suprabho04@gmail.com>
2024-08-18 01:05:14 +0530

    feat(repo): added berry template in the src

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,44 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+
+import { Provider } from 'react-redux';
+
+// project imports
+import App from './App';
+import store from './store/store';
+
+// google-fonts
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/700.css';
+
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
+
+// style + assets
+import 'src/assets/scss/style.scss';
+// import reportWebVitals from 'reportWebVitals';
+
+const container = document.getElementById('root');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+
+// ==============================|| REACT DOM RENDER  ||============================== //
+
+root.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+// reportWebVitals();
---
+++ src/store/store.js
@@ -0,0 +1,5 @@
+// third party
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from './reducer';
+const store = configureStore({ reducer });
+export default store;
